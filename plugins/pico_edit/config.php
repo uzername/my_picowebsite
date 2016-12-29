e17f4f1b4e5f8caa92e6a5f5fc5b23a1fe69ed5e
uzername <ivanpost777@gmail.com>
2016-12-30 00:04:12 +0200

    First Commit For New Website On PicoCMS

---
+++ plugins/pico_edit/config.php
@@ -0,0 +1,15 @@
+<?php
+
+global $backend_password;
+
+/*
+ * This should be a sha1 hash of your password.
+ * Use a tool like http://www.sha1-online.com to generate.
+ */
+//PicoCMSpass
+$backend_password = '16238194d51caae205fddcaac19007d121c353dd';
+
+$config['pico_edit_404'] = true;
+$config['pico_edit_options'] = true;           // Disallow options editing
+$config['pico_edit_default_author'] = 'Me';     // Default author for new pages
+?>
