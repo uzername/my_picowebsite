ffcb5e6a5f18b64e1d7a6368d40f7ec174229b87
uzername <ivanpost777@gmail.com>
2017-02-05 23:39:22 +0200

    Something

---
+++ plugins/TokenSecurity/TokenSecurity.php
@@ -0,0 +1,350 @@
+<?php
+
+/**
+ * 
+ * Jovan's Pico Security plugin. Place tokens in YAML and limit access to pages.
+ * This is the main file of plugin. it is used to dispatch reference to secured page, intercept the query 
+ * and render the propper sequrity message with input line for token entry. Another file should be created to receive entered tokens
+ * Some pages should not be viewed by everyone...
+ *
+ * @author  Daniel Rudolf and Jovan
+ * @link    http://picocms.org
+ * @license http://opensource.org/licenses/MIT The MIT License
+ * @version 1.0
+ */
+final class TokenSecurity extends AbstractPicoPlugin
+{
+    /**
+     * This plugin is enabled by default?
+     *
+     * @see AbstractPicoPlugin::$enabled
+     * @var boolean
+     */
+    protected $enabled = false;
+    
+    /**
+     * This plugin depends on ...
+     *
+     * @see AbstractPicoPlugin::$dependsOn
+     * @var string[]
+     */
+    protected $dependsOn = array();
+
+    public $TokenSecurityQueryFormPath = "";
+    private $TokenSecurityPageData=array();
+
+    /**
+     * Triggered after Pico has loaded all available plugins
+     *
+     * This event is triggered nevertheless the plugin is enabled or not.
+     * It is NOT guaranteed that plugin dependencies are fulfilled!
+     *
+     * @see    Pico::getPlugin()
+     * @see    Pico::getPlugins()
+     * @param  object[] &$plugins loaded plugin instances
+     * @return void
+     */
+    public function onPluginsLoaded(array &$plugins)
+    {
+        // your code
+    }
+
+    /**
+     * Triggered after Pico has read its configuration
+     *
+     * @see    Pico::getConfig()
+     * @param  array &$config array of config variables
+     * @return void
+     */
+    public function onConfigLoaded(array &$config)
+    {
+        // your code
+    }
+
+    /**
+     * Triggered after Pico has evaluated the request URL
+     *
+     * @see    Pico::getRequestUrl()
+     * @param  string &$url part of the URL describing the requested contents
+     * @return void
+     */
+    public function onRequestUrl(&$url)
+    {
+        // your code
+    }
+
+    /**
+     * Triggered after Pico has discovered the content file to serve
+     *
+     * @see    Pico::getBaseUrl()
+     * @see    Pico::getRequestFile()
+     * @param  string &$file absolute path to the content file to serve
+     * @return void
+     */
+    public function onRequestFile(&$file)
+    {
+        // your code
+    }
+
+    /**
+     * Triggered before Pico reads the contents of the file to serve
+     *
+     * @see    Pico::loadFileContent()
+     * @see    DummyPlugin::onContentLoaded()
+     * @param  string &$file path to the file which contents will be read
+     * @return void
+     */
+    public function onContentLoading(&$file)
+    {
+        // your code
+        error_log(date('Y-m-d H:m:s')." : "."onContentLoading"."\n", 3, __DIR__."/"."debug.log");
+        error_log(date('Y-m-d H:m:s')." : ".print_r($file,true)."\n", 3, __DIR__."/"."debug.log");
+
+    }
+
+    /**
+     * Triggered after Pico has read the contents of the file to serve
+     *
+     * @see    Pico::getRawContent()
+     * @param  string &$rawContent raw file contents
+     * @return void
+     */
+    public function onContentLoaded(&$rawContent)
+    {
+        // your code
+    }
+
+    /**
+     * Triggered before Pico reads the contents of a 404 file
+     *
+     * @see    Pico::load404Content()
+     * @see    DummyPlugin::on404ContentLoaded()
+     * @param  string &$file path to the file which contents were requested
+     * @return void
+     */
+    public function on404ContentLoading(&$file)
+    {
+        // your code
+    }
+
+    /**
+     * Triggered after Pico has read the contents of the 404 file
+     *
+     * @see    Pico::getRawContent()
+     * @param  string &$rawContent raw file contents
+     * @return void
+     */
+    public function on404ContentLoaded(&$rawContent)
+    {
+        // your code
+    }
+
+    /**
+     * Triggered when Pico reads its known meta header fields
+     *
+     * @see    Pico::getMetaHeaders()
+     * @param  string[] &$headers list of known meta header
+     *     fields; the array value specifies the YAML key to search for, the
+     *     array key is later used to access the found value
+     * @return void
+     */
+    public function onMetaHeaders(array &$headers)
+    {
+        // your code
+
+    }
+
+    /**
+     * Triggered before Pico parses the meta header
+     *
+     * @see    Pico::parseFileMeta()
+     * @see    DummyPlugin::onMetaParsed()
+     * @param  string   &$rawContent raw file contents
+     * @param  string[] &$headers    known meta header fields
+     * @return void
+     */
+    public function onMetaParsing(&$rawContent, array &$headers)
+    {
+        // your code
+    }
+
+    /**
+     * Triggered after Pico has parsed the meta header
+     *
+     * @see    Pico::getFileMeta()
+     * @param  string[] &$meta parsed meta data
+     * @return void
+     */
+    public function onMetaParsed(array &$meta)
+    {
+        // your code
+        error_log(date('Y-m-d H:m:s')." : "."onMetaParsed"."\n", 3, __DIR__."/"."debug.log");
+        error_log(date('Y-m-d H:m:s')." : ".print_r($meta, true)."\n", 3, __DIR__."/"."debug.log");
+        if (array_key_exists('usetoken', $meta) && ($meta['usetoken']==1)) {
+            error_log(date('Y-m-d H:m:s')." : "."UseToken found"."\n", 3, __DIR__."/"."debug.log");
+            $this->TokenSecurityPageData['UseToken'] = 'TRUE';
+
+        }
+    }
+
+    /**
+     * Triggered before Pico parses the pages content
+     *
+     * @see    Pico::prepareFileContent()
+     * @see    DummyPlugin::prepareFileContent()
+     * @see    DummyPlugin::onContentParsed()
+     * @param  string &$rawContent raw file contents
+     * @return void
+     */
+    public function onContentParsing(&$rawContent)
+    {
+        // your code
+        error_log(date('Y-m-d H:m:s')." : "."onContentParsing"."\n", 3, __DIR__."/"."debug.log");
+        error_log(date('Y-m-d H:m:s')." : ".print_r($rawContent, true)."\n", 3, __DIR__."/"."debug.log");
+    }
+
+    /**
+     * Triggered after Pico has prepared the raw file contents for parsing
+     *
+     * @see    Pico::parseFileContent()
+     * @see    DummyPlugin::onContentParsed()
+     * @param  string &$content prepared file contents for parsing
+     * @return void
+     */
+    public function onContentPrepared(&$content)
+    {
+        // your code
+        error_log(date('Y-m-d H:m:s')." : "."onContentPrepared"."\n", 3, __DIR__."/"."debug.log");
+        error_log(date('Y-m-d H:m:s')." : ".print_r($content, true)."\n", 3, __DIR__."/"."debug.log");
+        //probably better use Pico::getFileMeta()
+        if ((array_key_exists('UseToken', $this->TokenSecurityPageData))&&($this->TokenSecurityPageData['UseToken'] == 'TRUE')) {
+            //render here the token entry form
+            $renderPartial=file_get_contents($TokenSecurityQueryFormPath);
+            $renderPartial=$renderPartial."<input type=\"hidden\"></input>"
+            $renderPartial=$renderPartial."</form>";
+        }
+        
+        
+    }
+
+    /**
+     * Triggered after Pico has parsed the contents of the file to serve
+     *
+     * @see    Pico::getFileContent()
+     * @param  string &$content parsed contents
+     * @return void
+     */
+    public function onContentParsed(&$content)
+    {
+        // your code
+    }
+
+    /**
+     * Triggered before Pico reads all known pages
+     *
+     * @see    Pico::readPages()
+     * @see    DummyPlugin::onSinglePageLoaded()
+     * @see    DummyPlugin::onPagesLoaded()
+     * @return void
+     */
+    public function onPagesLoading()
+    {
+        // your code
+    }
+
+    /**
+     * Triggered when Pico reads a single page from the list of all known pages
+     *
+     * The `$pageData` parameter consists of the following values:
+     *
+     * | Array key      | Type   | Description                              |
+     * | -------------- | ------ | ---------------------------------------- |
+     * | id             | string | relative path to the content file        |
+     * | url            | string | URL to the page                          |
+     * | title          | string | title of the page (YAML header)          |
+     * | description    | string | description of the page (YAML header)    |
+     * | author         | string | author of the page (YAML header)         |
+     * | time           | string | timestamp derived from the Date header   |
+     * | date           | string | date of the page (YAML header)           |
+     * | date_formatted | string | formatted date of the page               |
+     * | raw_content    | string | raw, not yet parsed contents of the page |
+     * | meta           | string | parsed meta data of the page             |
+     *
+     * @see    DummyPlugin::onPagesLoaded()
+     * @param  array &$pageData data of the loaded page
+     * @return void
+     */
+    public function onSinglePageLoaded(array &$pageData)
+    {
+        // your code
+    }
+
+    /**
+     * Triggered after Pico has read all known pages
+     *
+     * See {@link DummyPlugin::onSinglePageLoaded()} for details about the
+     * structure of the page data.
+     *
+     * @see    Pico::getPages()
+     * @see    Pico::getCurrentPage()
+     * @see    Pico::getPreviousPage()
+     * @see    Pico::getNextPage()
+     * @param  array[]    &$pages        data of all known pages
+     * @param  array|null &$currentPage  data of the page being served
+     * @param  array|null &$previousPage data of the previous page
+     * @param  array|null &$nextPage     data of the next page
+     * @return void
+     */
+    public function onPagesLoaded(
+        array &$pages,
+        array &$currentPage = null,
+        array &$previousPage = null,
+        array &$nextPage = null
+    ) {
+        // your code
+    }
+
+    /**
+     * Triggered before Pico registers the twig template engine
+     *
+     * @return void
+     */
+    public function onTwigRegistration()
+    {
+        // your code
+        
+    }
+
+    /**
+     * Triggered before Pico renders the page
+     *
+     * @see    Pico::getTwig()
+     * @see    DummyPlugin::onPageRendered()
+     * @param  Twig_Environment &$twig          twig template engine
+     * @param  array            &$twigVariables template variables
+     * @param  string           &$templateName  file name of the template
+     * @return void
+     */
+    public function onPageRendering(Twig_Environment &$twig, array &$twigVariables, &$templateName)
+    {
+        // your code
+        //Appears that Twig_Environment &$twig object contains all the website content. Yikes!
+        //error_log(date('Y-m-d H:m:s')." : "."on Page Rendering! \n", 3, __DIR__."/"."debug.log");
+        //error_log(date('Y-m-d H:m:s')." : ".print_r($twig,true)."\n", 3, __DIR__."/"."debug.log");
+        //error_log(date('Y-m-d H:m:s')." : ".print_r($twigVariables,true)."\n", 3, __DIR__."/"."debug.log");
+        //error_log(date('Y-m-d H:m:s')." : ".print_r($templateName,true)."\n", 3, __DIR__."/"."debug.log");
+    }
+
+    /**
+     * Triggered after Pico has rendered the page
+     *
+     * @param  string &$output contents which will be sent to the user
+     * @return void
+     */
+    public function onPageRendered(&$output)
+    {
+        // your code
+        error_log(date('Y-m-d H:m:s')." : "."on Page Rendered! \n", 3, __DIR__."/"."debug.log");
+
+    }
+}
