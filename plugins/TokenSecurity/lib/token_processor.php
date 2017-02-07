9dc713f47d24ea631ffc6b9c27fea62baca81d8d
uzername <ivanpost777@gmail.com>
2017-02-07 11:31:39 +0200

    Administering and Fixing stuff. Better gitignore

--- plugins/TokenSecurity/lib/token_processor.php
+++
@@ -1,4 +0,0 @@
-<?php
-error_log(__FILE__.date(' Y-m-d H:m:s')." : ". "post query received" ."\n", 3, __DIR__."/"."debug.log");
-error_log(__FILE__.date(' Y-m-d H:m:s')." : ". print_r($POST) ."\n", 3, __DIR__."/"."debug.log");
-?>
